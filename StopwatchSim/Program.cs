3c716b7a35a743f0a6e6d750efd63c98d5fb003b
moranlow <moranlow@gmail.com>
2021-04-22 19:43:24 +0200

    Add project files.

---
+++ StopwatchSim/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopwatchSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            string response;
+            bool isStarted = true;
+
+            while (isStarted)
+            {
+
+                Console.WriteLine("Enter 'S' to start \nEnter 'P' to stop \nEnter 'R' to reset: \n");
+                response = Console.ReadLine().ToUpper();
+
+                if (response == "S")
+                {
+                    stopWatch.SetStart();
+                }
+                else if (response == "P")
+                {
+                    stopWatch.SetStop();
+                    Console.WriteLine(stopWatch.GetDuration().Hours + " hours, " +
+                        stopWatch.GetDuration().Minutes + " minutes, " +
+                        stopWatch.GetDuration().Seconds.ToString() + " seconds.\n");
+                }
+                else if (response == "R")
+                {
+                    stopWatch.Reset();
+                }
+                else
+                {
+                    throw new InvalidOperationException("Please write 'S', 'P' or 'R'");
+                }
+            }
+        }
+    }
+}
---
+++ StopwatchSim/Stopwatch.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopwatchSim
+{
+    class Stopwatch
+    {
+        private TimeSpan _duration;
+        private DateTime _start;
+        private DateTime _stop;
+        private bool _isRunning;
+
+        public Stopwatch()
+        {
+            _duration = new TimeSpan(0, 0, 0, 0, 0);
+            _isRunning = false;
+        }
+
+        public void SetDuration()
+        {
+            _duration = _duration + (this.GetStop() - this.GetStart());
+        }
+
+        public TimeSpan GetDuration()
+        {
+            return _duration;
+        }
+
+        public void SetStart()
+        {
+            if (_isRunning)
+                throw new InvalidOperationException("StopwatchAlreadyStartedException");
+
+            _start = DateTime.Now;
+            _isRunning = true;
+        }
+
+        public DateTime GetStart()
+        {
+            return _start;
+        }
+
+        public void SetStop()
+        {
+            if (!_isRunning)
+                throw new InvalidOperationException("Stopwatch is not running");
+            _stop = DateTime.Now;
+            this.SetDuration();
+            _isRunning = false;
+        }
+
+        public DateTime GetStop()
+        {
+            return _stop;
+        }
+
+        public void Reset()
+        {
+            _duration = new TimeSpan(0, 0, 0, 0);
+            _isRunning = false;
+        }
+    }
+}
